bd9c91c5bb8a58411d7ec4a046c2927e600efb81
Anonymous <none>
2013-12-13 21:54:34 -0800

    fix idle disconnects

--- mido.py
+++ mido.py
@@ -16,6 +16,11 @@ class Mido(irc.IRCClient):
         self.join(self.channel)
         if self.password:
             self.msg("NickServ", "IDENTIFY %s" % self.password)
+        self.repeatingPing(59)
+
+    def repeatingPing(self, delay):
+        reactor.callLater(delay, self.repeatingPing, delay)
+        self.ping(self.nickname)
 
     def privmsg(self, user, channel, msg):
         try:
